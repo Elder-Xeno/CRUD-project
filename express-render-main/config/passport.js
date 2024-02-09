08c4f075717757b73a8889b6823601528b277031
Gabby <gabzo24@live.com.au>
2024-02-10 09:38:48 +1100

    style footer

--- express-render-main/config/passport.js
+++ express-render-main/config/passport.js
@@ -9,7 +9,7 @@ passport.use(new GoogleStrategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK
+    callbackURL: process.env.GOOGLE_CALLBACK,
   },
   // The verify callback function...
   // Marking a function as an async function allows us
---
+++ express-render-main/public/scripts/footer.js
@@ -0,0 +1,16 @@
+// Get the button
+var mybutton = document.getElementById("backToTopButton");
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
